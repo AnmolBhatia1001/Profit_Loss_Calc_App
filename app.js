92c4fba74caa65530fcba44251ee76042e7f8cf1
AnmolSinghBhatia1001 <anmolsinghbhatia1001@gmail.com>
2022-09-14 18:10:04 +0530

    Style Added

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 
+const intial = document.querySelector("#intial");
+const qty = document.querySelector("#qty");
+const current = document.querySelector("#current");
+const checkBtn = document.querySelector("#checkBtn");
+const opDiv = document.querySelector("#opDiv");
+
 
 function calcProfitLoss(){
-    var intialPrice = 1000;
-    var unitQty = 1;
-    var currentPrice = 1111;
+    var intialPrice = intial.value;
+    var unitQty = qty.value;
+    var currentPrice = current.value;
 
-    var calcDiff = (currentPrice - intialPrice);
-    var calcPercentages = Math.round((calcDiff / intialPrice)*100);
+    var calcDiff = (currentPrice - intialPrice) * unitQty;
+    var calcPercentages = Math.round(((calcDiff / intialPrice)*100 )) ;
 
+
+    if(calcDiff > 0){
+        opDiv.innerText = `Hey, the profit is ${calcDiff}, & the percent is ${calcPercentages} %  `
+    }else{
+        opDiv.innerText = `Hey, the loss is ${Math. abs(calcDiff)} & the percent is ${Math. abs(calcPercentages)} %  `
+    }
+    
     console.log(calcDiff);
     console.log(calcPercentages);
 }
 
-calcProfitLoss()
+checkBtn.addEventListener("click", calcProfitLoss);
